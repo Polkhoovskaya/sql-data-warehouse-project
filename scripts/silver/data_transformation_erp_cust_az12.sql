eb52d1548d98e0fecac31dee1a99ddeb23d649af
Polkhoovskaya <37327952+Polkhoovskaya@users.noreply.github.com>
2026-05-29 17:53:39 +0300

    Add data_transformation_erp_cust_az12.sql
    This script performs data transformation on the erp_cust_az12 table, including checks for valid cid values, handling future birthdates, and standardizing gender entries before loading the cleaned data into the silver layer.

---
+++ scripts/silver/data_transformation_erp_cust_az12.sql
@@ -0,0 +1,107 @@
+SELECT 
+	cid,
+	bdate,
+	gen
+FROM [DataWarehouse].[bronze].[erp_cust_az12];
+
+-- Check that all cid values exist in the tables with primary keys
+-- Expectation: No Results
+
+SELECT 
+	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4)
+		ELSE cid
+	END AS cid,
+	bdate,
+	gen
+FROM [DataWarehouse].[bronze].[erp_cust_az12]
+WHERE CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4)
+		ELSE cid
+	END NOT IN (SELECT cst_key	FROM silver.crm_cust_info);
+---------------------------------------------------------------------------------------------------
+
+-- Check for very old customers
+-- Check for birthdays in the future
+
+SELECT 
+	cid,
+	bdate,
+	gen
+FROM [DataWarehouse].[bronze].[erp_cust_az12]
+WHERE bdate < '1926-01-01' OR bdate > GETDATE();
+
+-- Result:
+-- 23 rowse affected
+
+-- Solution
+-- Set all future values to NULL
+
+SELECT 
+	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4)
+		ELSE cid
+	END AS cid,
+	CASE WHEN bdate > GETDATE() THEN NULL
+		ELSE bdate
+	END AS	bdate,
+	gen
+FROM [DataWarehouse].[bronze].[erp_cust_az12];
+
+
+---------------------------------------------------------------------------------------------------
+
+-- Date Standardization & Consolidation
+
+SELECT DISTINCT
+gen
+from [DataWarehouse].[bronze].[erp_cust_az12];
+
+-- Result:
+--	gen
+--	NULL
+--	F
+--	 
+--	Male
+--	Female
+--	M 
+
+
+
+-- Solution
+-- Leave only Male, Female and n/a
+
+
+SELECT 
+	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4)
+		ELSE cid
+	END AS cid,
+	CASE WHEN bdate > GETDATE() THEN NULL
+		ELSE bdate
+	END AS	bdate,
+	CASE WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+		WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+		ELSE 'n/a'
+	END AS	gen
+FROM [DataWarehouse].[bronze].[erp_cust_az12];
+
+
+---------------------------------------------------------------------------------------------------
+
+-- Load erp_cust_az12 to silver
+
+
+INSERT INTO silver.erp_cust_az12 (
+	cid,
+	bdate,
+	gen
+)
+SELECT 
+	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4)
+		ELSE cid
+	END AS cid,
+	CASE WHEN bdate > GETDATE() THEN NULL
+		ELSE bdate
+	END AS	bdate,
+	CASE WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+		WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+		ELSE 'n/a'
+	END AS	gen
+FROM [DataWarehouse].[bronze].[erp_cust_az12];
