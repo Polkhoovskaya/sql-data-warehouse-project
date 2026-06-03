c86e9e0044d7d54d23288b6e2d32373c9e797c73
Polkhoovskaya <37327952+Polkhoovskaya@users.noreply.github.com>
2026-06-03 09:11:50 +0300

    Add data_transformation_erp_loc_a101.sql
    This SQL script performs data transformation for the erp_loc_a101 table, ensuring data standardization and consistency before loading it into the silver layer.

---
+++ scripts/silver/data_transformation_erp_loc_a101.sql
@@ -0,0 +1,58 @@
+SELECT CID, CNTRY
+FROM bronze.erp_loc_a101;
+
+-- Check that all cid values exist in the tables with primary keys
+-- Expectation: No Results
+
+SELECT REPLACE(CID, '-', '') CID
+FROM bronze.erp_loc_a101
+WHERE REPLACE(CID, '-', '') NOT IN ( SELECT cst_key
+FROM silver.crm_cust_info);
+
+-- No Results
+
+---------------------------------------------------------------------------------------------------
+
+-- Data Standardization & Consistency
+
+SELECT DISTINCT CNTRY
+FROM bronze.erp_loc_a101
+ORDER BY CNTRY;
+
+-- Result:
+--	CNTRY
+--	NULL
+--	  
+--	Australia
+--	Canada
+--	DE
+--	France
+--	Germany
+--	United Kingdom
+--	United States
+--	US
+--	USA
+
+SELECT REPLACE(CID, '-', '') CID,
+CASE WHEN TRIM(CNTRY) = 'DE' THEN 'Germany'
+	 WHEN TRIM(CNTRY) IN ('US', 'USA') THEN 'United States'
+	 WHEN CNTRY IS NULL OR TRIM(CNTRY) = '' THEN 'n/a'
+	 ELSE TRIM(CNTRY)
+END AS CNTRY
+FROM bronze.erp_loc_a101;
+
+---------------------------------------------------------------------------------------------------
+
+-- Load erp_loc_a101 to silver
+
+INSERT INTO silver.erp_loc_a101 (
+	CID, 
+	CNTRY
+)
+SELECT REPLACE(CID, '-', '') CID,
+CASE WHEN TRIM(CNTRY) = 'DE' THEN 'Germany'
+	 WHEN TRIM(CNTRY) IN ('US', 'USA') THEN 'United States'
+	 WHEN CNTRY IS NULL OR TRIM(CNTRY) = '' THEN 'n/a'
+	 ELSE TRIM(CNTRY)
+END AS CNTRY
+FROM bronze.erp_loc_a101;
